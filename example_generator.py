6c1340bb3c71dade77a5c6c5e84b06b591b35212
galmeriol <imzafer@yahoo.com>
2020-05-25 16:01:07 +0300

    added wheel

--- example_generator.py
+++ example_generator.py
@@ -3,27 +3,24 @@ from components.newsletter_item import NewsletterItem
 from components.newsletter_item_link import NewsletterItemLink
 from components.newsletter import Newsletter
 
-sections = {"1": "Etkinlikler ve önemli duyurular"}
+sections = {"1": "Etkinlikler ve önemli duyurular",
+            "2": "Güncel Çalışma Alanları"}
+
+
 
 nlitem = NewsletterItem(
-    header="Apache Spark 3.0 Preview Release çıktı", 
-    header_link="https://spark.apache.org/news/spark-3.0.0-preview.html",
+    header="Beyin aktivitelerini kelimelere dönüştüren algoritma çalışmaları devam ediyor", 
+    header_link="https://www.bbc.com/news/science-environment-52094111",
     content="""
-    Dağıtık veri işlemci kütüphanesi Apache Spark yeni majör versiyonu 3.0'ı geliştiricilerin test etmesi amacıyla yayınladı. Bu versiyonda;
-          <ul>
-            <li>Graph tabanlı veritabanı sorgulama dili <b>Cypher</b> desteği eklendi.</li>
-            <li>Python 3'e tam geçiş bekleniyor.</li>
-            <li>Scala 2.12 ve Java 11 desteği geldi.</li>
-            <li>RDD üzerindeki işlemler ve UDF'ler için GPU desteği geldi.</li>
-          </ul>
+        Beyin aktivitelerini kelimelere dönüştürmek oldukça eski bir araştırma alanı. California Üniversitesi'nden bir grup araştırmacı küçük bir kelime ve cümle grubu ile 3\% hata oranını yakayabildiler.
     """,
     links=[
-        NewsletterItemLink(text="Yeni özellikler", link="https://towardsdatascience.com/glimpse-into-spark-3-0"),
-        NewsletterItemLink(text="Cypher Query Language", link="https://neo4j.com/developer/cypher-query-language/")
+        NewsletterItemLink(text="Makale", link="https://www.nature.com/articles/s41593-020-0608-8"),
+        NewsletterItemLink(text="İlgili Github Repo", link="https://github.com/jgmakin/machine_learning")
         ],
-    image="https://www.computing.co.uk/w-images/cc6f36ae-ffb1-4271-8847-725556046f5c/0/apachesparklogo-580x358.png",
-    image_link="https://spark.apache.org/news/spark-3.0.0-preview.html",
-    section="1")
+    image="https://ichef.bbci.co.uk/news/660/cpsprodpb/14331/production/_111473728_f0194286-brain_neural_network_illustration.jpg",
+    image_link="https://www.bbc.com/news/science-environment-52094111",
+    section="2")
 
 Newsletter(
     page_title="Teknolojik Gelişmeler",
