e4cdb598fc63ccc09925ecaba42f39409f4d66c5
zafer <galmeriol@users.noreply.github.com>
2020-07-22 08:53:17 +0300

    Update template_mixin.py

--- mixin/template_mixin.py
+++ mixin/template_mixin.py
@@ -8,6 +8,7 @@ class TemplateMixin():
     def load_template(self, file_name):
         
         template_dir = dirname(dirname(abspath(__file__))).rstrip('/') + '/templates'
+        print(template_dir)
         templateLoader = jinja2.FileSystemLoader(searchpath=template_dir)
         templateEnv = jinja2.Environment(loader=templateLoader)
         TEMPLATE_FILE = file_name
