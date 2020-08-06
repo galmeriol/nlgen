c073b4273ce513178af0535381d16045392d10f5
ZaferEr <zaferer@garantibbva.com.tr>
2020-08-06 08:50:11 +0300

    .

--- mixin/template_mixin.py
+++ mixin/template_mixin.py
@@ -8,7 +8,6 @@ class TemplateMixin():
     def load_template(self, file_name):
         
         template_dir = dirname(dirname(abspath(__file__))).rstrip('/') + '/templates'
-        print(template_dir)
         templateLoader = jinja2.FileSystemLoader(searchpath=template_dir)
         templateEnv = jinja2.Environment(loader=templateLoader)
         TEMPLATE_FILE = file_name
@@ -16,7 +15,6 @@ class TemplateMixin():
         return templateEnv.get_template(TEMPLATE_FILE)
 
     def render_object(self, template, **kwargs):
-        print(template)
         template_str = template.render(**kwargs)
 
         return template_str
